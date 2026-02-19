d419343e116e71c7f0b4186abdbb9256e89553d9
ismailramjean <ismailramjean@gmail.com>
2026-02-19 06:42:37 +0400

    - count down timer

--- src/app/components/countdown/countdown.component.ts
+++ src/app/components/countdown/countdown.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, computed, OnDestroy, OnInit, signal} from '@angular/core';
+import {DatePipe} from '@angular/common';
 
 @Component({
   selector: 'app-countdown',
-  imports: [],
   templateUrl: './countdown.component.html',
   styleUrl: './countdown.component.scss'
 })
-export class CountdownComponent {
+export class CountdownComponent implements OnInit, OnDestroy {
 
+  now = signal(new Date());
+
+  hours = computed(() => this.now().getHours().toString().padStart(2, '0'));
+  minutes = computed(() => this.now().getMinutes().toString().padStart(2, '0'));
+  seconds = computed(() => this.now().getSeconds().toString().padStart(2, '0'));
+
+  private _timerId: any;
+
+  ngOnInit(): void {
+    this._timerId = setInterval(() => {
+      this.now.set(new Date());
+    }, 500);
+  }
+
+  ngOnDestroy(): void {
+    if (this._timerId) {
+      clearInterval(this._timerId);
+    }
+  }
 }
